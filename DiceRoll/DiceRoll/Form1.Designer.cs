4920c8e034ae24d1056c5b01db8f7a2467e4938a
Richard <r.schlueter@outlook.com>
2013-02-14 22:24:17 -0800

    Dice Roll

---
+++ DiceRoll/DiceRoll/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class DiceRoll
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRoll));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultsBox = new System.Windows.Forms.ListBox();
+            this.LoadBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.AvgBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(146, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dice Roll";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("OpenSymbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(153, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "The official dice roll generator...";
+            // 
+            // ResultsBox
+            // 
+            this.ResultsBox.BackColor = System.Drawing.Color.Black;
+            this.ResultsBox.Font = new System.Drawing.Font("OpenSymbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultsBox.ForeColor = System.Drawing.Color.White;
+            this.ResultsBox.FormattingEnabled = true;
+            this.ResultsBox.ItemHeight = 16;
+            this.ResultsBox.Location = new System.Drawing.Point(12, 147);
+            this.ResultsBox.Name = "ResultsBox";
+            this.ResultsBox.Size = new System.Drawing.Size(292, 420);
+            this.ResultsBox.TabIndex = 3;
+            // 
+            // LoadBox
+            // 
+            this.LoadBox.BackColor = System.Drawing.Color.Black;
+            this.LoadBox.Font = new System.Drawing.Font("OpenSymbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadBox.ForeColor = System.Drawing.Color.White;
+            this.LoadBox.FormattingEnabled = true;
+            this.LoadBox.ItemHeight = 16;
+            this.LoadBox.Location = new System.Drawing.Point(336, 147);
+            this.LoadBox.Name = "LoadBox";
+            this.LoadBox.Size = new System.Drawing.Size(304, 276);
+            this.LoadBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("OpenSymbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(153, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(300, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Press the roll button to simulate 100 dice rolls!";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(475, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 60);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Roll Dice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(71, 573);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 60);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Write To File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(410, 573);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 60);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Read From File";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AvgBox
+            // 
+            this.AvgBox.BackColor = System.Drawing.Color.Black;
+            this.AvgBox.Font = new System.Drawing.Font("OpenSymbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AvgBox.ForeColor = System.Drawing.Color.White;
+            this.AvgBox.FormattingEnabled = true;
+            this.AvgBox.ItemHeight = 16;
+            this.AvgBox.Location = new System.Drawing.Point(337, 451);
+            this.AvgBox.Name = "AvgBox";
+            this.AvgBox.Size = new System.Drawing.Size(303, 116);
+            this.AvgBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("OpenSymbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(334, 432);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Result Averages";
+            // 
+            // DiceRoll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(651, 720);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AvgBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LoadBox);
+            this.Controls.Add(this.ResultsBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DiceRoll";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dice Roll";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox ResultsBox;
+        private System.Windows.Forms.ListBox LoadBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox AvgBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DiceRoll/DiceRoll/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class DiceRoll : Form
+    {
+
+        private Random roll = new Random();
+        private int dice1;
+        private int dice2;
+        private int[] sum;
+        private int i;
+        private string Entry;
+        private int count;
+        private int count2;
+        private int count3;
+        private int[] average;
+        private string[] averageCalc;
+        private int[] resultCount;
+        private bool found;
+        private int index;
+        private int track;
+        private int numbercount;
+
+        public DiceRoll()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Array to hold the sums of the dice rolls, array used to make file writing easier as it occurs ouside the for loop
+            this.sum = new int[100];
+            this.average = new int[100];
+
+            // Ensures only one result set is present at a time
+            ResultsBox.Items.Clear();
+
+            // Simple iteration from 1 to 100
+            for (i = 1; i < 101; i++)
+            {
+
+                // dice1 and dice2 are random numbers ranging from 1 and 6
+                this.dice1 = roll.Next(1, 7);
+                this.dice2 = roll.Next(1, 7);
+
+                // calculates sum of the numbers
+                this.sum[this.i - 1] = this.dice1 + this.dice2;
+                this.average[this.i - 1] = this.sum[this.i - 1];
+
+                // The if statements are only to properly align the data in the text box... OCD, I know...
+                // 1 digit numbers
+                if (this.i < 10)
+                {
+
+                    ResultsBox.Items.Add("Dice Roll " + this.i + ":  (" + this.dice1 + "," + this.dice2 + ") - Result = " + this.sum[this.i - 1]);
+
+                }
+
+                // 2 digit numbers
+                else
+                {
+
+                    ResultsBox.Items.Add("Dice Roll " + this.i + ": (" + this.dice1 + "," + this.dice2 + ") - Result = " + this.sum[this.i - 1]);
+
+                }
+
+            }
+
+        }
+
+        // Event handler that saves the data to a text file
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // If the list box has items, save to file
+            if (ResultsBox.Items.Count > 0)
+            {
+
+                using (StreamWriter outputfile = new StreamWriter("roll.txt"))
+                {
+
+                    int i;
+                    for (i = 0; i < 100; i++)
+                    {
+                        // Write array contents to file
+                        outputfile.WriteLine(this.sum[i]);
+                    }
+
+                    outputfile.Close();
+
+                }
+
+                using (StreamWriter outputfile = new StreamWriter("rollAverage.txt"))
+                {
+
+                    int i;
+                    for (i = 0; i < 100; i++)
+                    {
+                        // Write array contents to file
+                        outputfile.WriteLine(this.average[i]);
+                    }
+
+                    outputfile.Close();
+
+                }
+
+                Success form = new Success();
+
+                form.ShowDialog();
+
+                // Clear the list box after saving
+                ResultsBox.Items.Clear();
+
+            }
+
+            // If no items are present, show error dialog
+            else
+            {
+
+                Error message = new Error();
+
+                message.ShowDialog();
+
+            }
+        }
+
+        // Loads the data into the second list box if the roll.txt file is present
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            this.count = 1;
+            this.count2 = 2;
+            this.averageCalc = new string[100];
+            this.resultCount = new int[12];
+
+            LoadBox.Items.Clear();
+            AvgBox.Items.Clear();
+
+            if (File.Exists("roll.txt"))
+            {
+
+                using (StreamReader inputfile = new StreamReader("roll.txt"))
+                {
+
+                    while (!inputfile.EndOfStream)
+                    {
+
+                        // one digit
+                        if (count < 10)
+                        {
+
+                            this.Entry = inputfile.ReadLine();
+                            LoadBox.Items.Add("Dice Roll " + this.count + ": " + " Result = " + this.Entry);
+                            this.count++;
+
+                        }
+
+                        // two digit
+                        else
+                        {
+
+                            this.Entry = inputfile.ReadLine();
+                            LoadBox.Items.Add("Dice Roll " + this.count + ": " + "Result = " + this.Entry);
+                            this.count++;
+
+                        }
+
+                    }
+
+                    inputfile.Close();
+
+                }
+
+            }
+
+            if (File.Exists("rollAverage.txt"))
+            {
+
+                this.count = 0;
+                this.index = 0;
+                this.count3 = 1;
+                this.numbercount = 0;
+
+                for (i = 1; i < 12; i++)
+                {
+
+                    resultCount[i - 1] = i + 1;
+
+                }
+
+                using (StreamReader inputfile = new StreamReader("rollAverage.txt"))
+                {
+
+                    while (!inputfile.EndOfStream)
+                    {
+                            this.Entry = inputfile.ReadLine();
+                            this.averageCalc[this.count] = this.Entry;
+                            this.count++;
+
+                    }
+
+                    inputfile.Close();
+
+                    
+
+                }
+
+                while (this.index < this.averageCalc.Length)
+                {
+
+                    if (this.averageCalc[this.index] == "2")
+                    {
+
+                        this.resultCount[0]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "3")
+                    {
+
+                        this.resultCount[1]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "4")
+                    {
+
+                        this.resultCount[2]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "5")
+                    {
+
+                        this.resultCount[3]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "6")
+                    {
+
+                        this.resultCount[4]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "7")
+                    {
+
+                        this.resultCount[5]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "8")
+                    {
+
+                        this.resultCount[6]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "9")
+                    {
+
+                        this.resultCount[7]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "10")
+                    {
+
+                        this.resultCount[8]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "11")
+                    {
+
+                        this.resultCount[9]++;
+
+                    }
+
+                    if (this.averageCalc[this.index] == "12")
+                    {
+
+                        this.resultCount[10]++;
+
+                    }
+
+
+
+                    this.index++;
+
+                }
+
+
+
+
+                while (this.count2 < 13)
+                {
+                
+                        AvgBox.Items.Add("Dice Result (" + this.count2 + ") Occurances: " + (this.resultCount[this.count2 - 2] - this.count2));
+                        this.count2++;
+
+                }
+
+            }
+
+            else
+            {
+
+                Error message = new Error();
+
+                message.ShowDialog();
+
+            }
+        }
+    }
+}
